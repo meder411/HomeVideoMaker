4ed4e9138e3dd4b51d21ac7e46c4759ad26d07b0
Marc Eder <6818607+meder411@users.noreply.github.com>
2021-01-17 14:54:21 -0500

    initial commit

---
+++ merge_videos.py
@@ -0,0 +1,186 @@
+"""
+Author: Marc Eder
+Description: A tool to merge captured video clips into longer home movies with camcorder-style data/time overlays
+"""
+
+
+import os.path as osp
+import argparse
+import glob
+import exiftool
+from util import *
+
+# Currently accepts these types
+supported_input_types = (".mp4", ".mov", ".avi")
+
+# Keys to try in the EXIF data in order of precedence
+timestamp_keys = [
+    "QuickTime:DateTimeOriginal", "QuickTime:CreationDate",
+    "QuickTime:CreateDate"
+]
+
+
+def parse_arguments() -> argparse.Namespace:
+    """
+    Create and parse the command line arguments
+    """
+    # Create arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input-dir",
+        "-i",
+        type=str,
+        required=True,
+        help="Path to directory containing clips to merge",
+    )
+    parser.add_argument(
+        "--output-dir",
+        "-o",
+        type=str,
+        required=True,
+        help="Where the output video is stored",
+    )
+    parser.add_argument(
+        "--num-parts",
+        "-n",
+        type=int,
+        default=1,
+        help="Number of movies to merge these clips into",
+    )
+    parser.add_argument("--no-date",
+                        action="store_false",
+                        default=True,
+                        help="Disables date overlay")
+    parser.add_argument(
+        "--verbosity",
+        "-v",
+        type=str,
+        default="quiet",
+        help="ffmpeg verbosity level",
+    )
+    parser.add_argument("--debug-cmd",
+                        action="store_true",
+                        default=False,
+                        help="Prints ffmpeg command")
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Return namespace wrapper of arguments
+    return args
+
+
+def main(args) -> None:
+    """
+    Main operation
+    """
+
+    # Parse the arguments
+    # Output filename is the input directory name
+    out_fname = osp.join(args.output_dir,
+                         osp.basename(args.input_dir.rstrip("/")) + ".mp4")
+    debug = args.debug_cmd  # Whether to print the ffmpeg command
+    num_parts = args.num_parts  # Number of movies to merge clips into
+    overlay_clock = args.no_date  # Whether to overlay the clock on the image
+    verbosity = args.verbosity  # FFMPEG verbosity level
+
+    # Get all files in the provided directory
+    files = glob.glob(osp.join(args.input_dir, "*"))
+
+    # Cull the files only to supported inputs
+    clips = []
+    for file in files:
+        ext = osp.splitext(file)[1]
+        if not ext.lower() in supported_input_types:
+            print(
+                f"`{osp.splitext(file)[1]}` file format not supported. `{file}` will be skipped!"
+            )
+        else:
+            clips.append(file)
+
+    # Obtain the exif data for each video
+    movies = []
+    with exiftool.ExifTool() as exif:
+        for vid in clips:
+            # Get all the metadata
+            exif_data = exif.get_metadata(vid)
+
+            # Extract date info from the metadata
+            date_info = get_date_info(exif_data)
+
+            if date_info is None:
+                # If none of the keys are found, warn but add the video anyway
+                print(
+                    f"Failed to find creation timestamp key in EXIF data for video `{vid}`! No timestamp will be added and clip may be sorted out of order."
+                )
+
+            # Create a Movie recordtype and add it to the list
+            movies.append(
+                Movie(fname=vid,
+                      height=exif_data["QuickTime:ImageHeight"],
+                      width=exif_data["QuickTime:ImageWidth"],
+                      create_date=date_info,
+                      rotation=exif_data["Composite:Rotation"]))
+
+    # Sort by creation date, ascending
+    movies.sort(key=lambda x: x.create_date)
+
+    # Check for any duplicate timestamps, which may indicate redundant clips
+    print("Checking for potential duplicate clips...........")
+    duplicate_check(movies)
+    print("Done!")
+
+    # Check all clips have audio streams
+    print("\n\nChecking that all clips have audio streams.........")
+    check_audio(movies)
+    print("Done!")
+
+    # Confirm user wants to continue
+    user_in = ""
+    while user_in != "yes":
+        user_in = input("Do you want to continue merging (yes/no)?: ")
+
+        if user_in == "no":
+            print("Exiting!")
+            exit()
+
+    # Split movies based on number of parts desired
+    if num_parts > 1:
+
+        # How many clips to put in each merged movie
+        step = len(movies) // num_parts
+
+        # For each movie to create
+        for i in range(num_parts):
+            # Grab a subset of the temporally ordered movies
+            start = i * step
+            end = (i + 1) * step if i < num_parts - 1 else len(movies)
+            movies_subset = movies[start:end]
+
+            # Add numerical index to output files
+            filename, ext = osp.splitext(out_fname)
+            indexed_out_fname = f"{filename}{i}{ext}"
+
+            # Build the concatenation command by parsing the movie info
+            concat_cmd = create_concat_cmd(movies_subset, indexed_out_fname,
+                                           overlay_clock, args.verbosity)
+            if args.debug_cmd:
+                print(concat_cmd)
+            run_cmd(concat_cmd)
+
+    else:
+        # Build the concatenation command by parsing the movie info
+        concat_cmd = create_concat_cmd(movies, out_fname, overlay_clock,
+                                       args.verbosity)
+        if args.debug_cmd:
+            print(concat_cmd)
+        run_cmd(concat_cmd)
+
+
+# =========================================================================== #
+# ================================== MAIN =================================== #
+# =========================================================================== #
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
---
+++ util.py
@@ -0,0 +1,230 @@
+from typing import Tuple, List
+import subprocess
+import datetime
+from recordtype import recordtype
+
+# Movie type (essentially a mutable namedtuple)
+Movie = recordtype("Movie", "fname height width create_date rotation")
+
+
+def run_cmd(cmd: str, *args, **kwargs) -> subprocess.CompletedProcess:
+    """
+    Simple wrapper around subprocess run
+    """
+    return subprocess.run(cmd, *args, shell=True, check=True, **kwargs)
+
+
+def parse_datetime_with_tz(exif_date: str) -> datetime.datetime:
+    """
+    Parses the date/time captured from the exif data, assuming timezone is already incorporated
+    """
+    date, tod = (exif_date.split(" "))
+    tod = tod.split("-")[0]
+    yr, mo, day = date.split(":")
+    hour, minute, sec = tod.split(":")
+    try:
+        d = datetime.date(int(yr), int(mo), int(day))
+        t = datetime.time(int(hour), int(minute), int(sec))
+    except ValueError:
+        print("Invalid date/time information")
+        return None
+    return datetime.datetime.combine(d, t)
+
+
+def get_date_info(exif_data: dict) -> datetime.datetime:
+    """
+    Parses the exif data from each video looking for the creation date/time keys
+    """
+
+    # Look for the creation date/time keys in the metadata
+    timestamp_val = None
+    timestamp_key = None
+    i = 0
+    while timestamp_val is None and i < len(timestamp_keys):
+        timestamp_key = timestamp_keys[i]
+        timestamp_val = exif_data.get(timestamp_key)
+        i += 1
+
+    # If/else statements disambiguating how to parse each case
+    date_info = None
+    if timestamp_key in [
+            "QuickTime:DateTimeOriginal", "QuickTime:CreationDate"
+    ]:
+        # In this class of tags, the timezone data is incorporated into the date already but also attached to the end of the date string
+        date_info = parse_datetime_with_tz(timestamp_val)
+    elif timestamp_key in ["QuickTime:CreateDate"]:
+        # This class of tags has an additional tag to reference for the timezone. We can use the same function to get the UTC datetime
+        date_info = parse_datetime_with_tz(timestamp_val)
+
+        # If there"s a time zone, adjust accordingly
+        if "QuickTime:TimeZone" in exif_data:
+            tz_offset = exif_data["QuickTime:TimeZone"]
+            date_info += datetime.timedelta(minutes=tz_offset)
+
+    # Returns date information (or None if not date info found)
+    return date_info
+
+
+def create_concat_cmd(movies: List[Movie],
+                      out_fname: str,
+                      max_dims: Tuple[int, int] = (1920, 1080),
+                      overlay_clock: bool = True,
+                      verbosity) -> str:
+    cmd = "ffmpeg \\\n"
+    cmd += "    -y \\\n"
+    cmd += f"    -loglevel {verbosity} \\\n"
+    cmd += "    -stats \\\n"
+
+    n = len(movies)
+    for mov in movies:
+        cmd += f"    -i {mov.fname} \\\n"
+
+    # Create the string that pulls the relevant streams from the inputs. Assumes all inputs have 1 audio and 1 video stream
+    complex_filter_str = "\""
+    for i, mov in enumerate(movies):
+        filter_str = create_filter_str(mov, max_dims, overlay_clock)
+        complex_filter_str += f"[{i}:v]{filter_str}[v{i}]; \\\n"
+
+    for i, mov in enumerate(movies):
+        complex_filter_str += f"[v{i}]"
+        complex_filter_str += f"[{i}:a]"
+
+    # Finish the command string with the number of streams to join (n) and declare that there will always be 1 audio and 1 video output stream
+    complex_filter_str += f"concat=n={n}:v=1:a=1[v][a]\""
+
+    cmd += f"    -filter_complex \\\n{complex_filter_str} \\\n"
+
+    # Map the concatenated streams to the output file
+    cmd += "    -map \"[v]\" \\\n"
+    cmd += "    -map \"[a]\" \\\n"
+    cmd += "    -vcodec libx264 \\\n"
+    cmd += "    -acodec aac \\\n"
+
+    # Add the output file
+    cmd += f"    {out_fname}"
+
+    return cmd
+
+
+def swap_dims(movie: Movie) -> None:
+    """
+    Simply swaps the height and width attributes for a movie. Called with rotation is not 0.
+    """
+    tmp = movie.width
+    movie.width = movie.height
+    movie.height = tmp
+
+
+def create_filter_str(movie: Move,
+                      max_dims: Tuple[int, int],
+                      overlay_clock: bool = True) -> str:
+    # Open filter string
+    filter_str = "setpts=PTS-STARTPTS, "
+
+    # Match dimensions to rotated content
+    if movie.rotation != 0:
+        if movie.rotation == 90:
+            swap_dims(movie)
+            movie.rotation = 0
+        elif movie.rotation == 270:
+            swap_dims(movie)
+            movie.rotation = 0
+
+    # Scale to the max dims (assume the input images are no worse than 1280 x 720). Warn if video dimensions below that.
+    if ((movie.width > movie.height) and
+        ((movie.width < 1280) or
+         (movie.height < 720))) or ((movie.width <= movie.height) and
+                                    ((movie.height < 1280) or
+                                     (movie.width < 720))):
+        print(
+            f"`{movie.fname}` is less than (1280x720) resolution. Results may look pixelated."
+        )
+
+    # Do nothing when:
+    # * both dimensions are correct
+    # * one dimension is correct and the other is smaller
+    scale = False
+    # Case 1: Downscale height, adjust width accordingly (will pad)
+    if (movie.width < max_dims[0]) and (movie.height > max_dims[1]):
+        height_ratio = movie.height / max_dims[1]
+        movie.height = max_dims[1]
+        movie.width = int(movie.width / height_ratio)
+        scale = True
+    # Case 2: Downscale width, adjust height accordingly (will pad)
+    elif (movie.width > max_dims[0]) and (movie.height < max_dims[1]):
+        width_ratio = movie.width / max_dims[0]
+        movie.width = max_dims[0]
+        movie.height = int(movie.height / width_ratio)
+        scale = True
+    # Case 3: Upscale
+    elif (movie.width < max_dims[0]) and (movie.height < max_dims[1]):
+        width_ratio = movie.width / max_dims[0]
+        height_ratio = movie.height / max_dims[1]
+        # Upscale based on width
+        if width_ratio >= height_ratio:
+            movie.width = max_dims[0]
+            movie.height = int(movie.height / width_ratio)
+            scale = True
+        # Upscale based on height
+        else:
+            movie.height = max_dims[1]
+            movie.width = int(movie.width / height_ratio)
+            scale = True
+    # Case 4: Downscale
+    elif (movie.width < max_dims[0]) and (movie.height < max_dims[1]):
+        width_ratio = movie.width / max_dims[0]
+        height_ratio = movie.height / max_dims[1]
+        # Downscale based on width
+        if width_ratio >= height_ratio:
+            movie.width = max_dims[0]
+            movie.height = int(movie.height / width_ratio)
+            scale = True
+        # Downscale based on height
+        else:
+            movie.height = max_dims[1]
+            movie.width = int(movie.width / height_ratio)
+            scale = True
+
+    # Add the rescale filter
+    if scale:
+        filter_str += f"scale={movie.width}:{movie.height}, "
+
+    # Pad the videos to make them all same size
+    if (movie.width < max_dims[0]) or (movie.height < max_dims[1]):
+        filter_str += f"pad=width={max_dims[0]}:height={max_dims[1]}:x={(max_dims[0] - movie.width) // 2}:y={(max_dims[1] - movie.height) // 2}:color=black, "
+
+    filter_str += "setsar=1, "
+
+    # Overlay the clock
+    if movie.create_date is not None and overlay_clock:
+        filter_str += f"drawtext=expansion=strftime: basetime=$(date +%s -d\"{movie.create_date.date()} {movie.create_date.time()}\")000000 : fontcolor=white : text=\"%^b %d, %Y%n%l\\\\:%M%p\" : fontsize=36 : y={movie.height}-4*lh : x={ movie.width + (max_dims[0] - movie.width) // 2}-text_w-2*max_glyph_w"
+
+    return filter_str
+
+
+def duplicate_check(movies: List[Movie]) -> None:
+    """
+    Warns user of possible duplicate clips when multiple clips have the same exact timestamp (to the tenth of a second).
+    """
+    unique_time = {}
+    for m in movies:
+        if m.create_date.timestamp() not in unique_time:
+            unique_time[m.create_date.timestamp()] = m
+        else:
+            print(
+                f"ALERT: Possible duplicate video found: {m.fname} and {unique_time[m.create_date.timestamp()].fname} share the same timestamp!"
+            )
+
+
+def check_audio(movies: List[Movie]) -> None:
+    """
+    Calls ffprobe on each clip to confirm that there is an audio stream. If one doesn't exist, alerts the user.
+    """
+    for m in movies:
+        output = run_cmd(
+            f"ffprobe -i {m.fname} -show_streams -select_streams a -loglevel error",
+            capture_output=True)
+        if not output.stdout:
+            print(
+                f"ALERT: Video missing audio stream! Merging will fail! ({m.fname})"
+            )
